472ffa69b8c18012206d2fbf7107094ff0f66c1b
Ariya Sree <W0849647@myscc.ca>
2024-09-26 12:46:02 +0530

    Components created

---
+++ src/app/food-info/food-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-info.component.html',
+  styleUrl: './food-info.component.css'
+})
+export class FoodInfoComponent {
+
+}
