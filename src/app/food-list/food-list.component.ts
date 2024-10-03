2113037f472e5a2b2ff722920f15032820da429a
Ariya Sree <W0849647@myscc.ca>
2024-10-03 19:21:46 -0400

    created dataMock and services

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -15,12 +15,5 @@ import {NgForOf} from "@angular/common";
 })
 export class FoodListComponent {
   displayedlists:string[]= ['ProductId','Brand','ItemName','ExpiryDate','Barcode'];
-  FoodList : FoodProduction[] = [
-    {ProductId: 1, Brand: "Nirapara", ItemName: "Rice", ExpiryDate: "10-03-2027", Barcode: 1234},
-    {ProductId: 2, Brand: "DoubleHourse", ItemName: "Iddiyappam", ExpiryDate: "01-11-2025", Barcode: 1235},
-    {ProductId: 3, Brand: "Shah", ItemName: "ChilliPowder", ExpiryDate: "03-03-2025", Barcode: 1236},
-    {ProductId: 4, Brand: "SK", ItemName: "Salt", ExpiryDate: "05-09-2025", Barcode: 1237}
-
-  ];
 
 }
