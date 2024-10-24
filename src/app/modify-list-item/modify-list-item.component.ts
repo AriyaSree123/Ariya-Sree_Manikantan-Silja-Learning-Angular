8850b2d719954d67d964d5168a55b2a5af54ae71
Ariya Sree <W0849647@myscc.ca>
2024-10-24 18:40:11 -0400

    Added Modify list and images

---
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {FoodList} from "../shared/models/dataMock-food";
+
+@Component({
+  selector: 'app-modify-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-list-item.component.html',
+  styleUrl: './modify-list-item.component.css'
+})
+export class ModifyListItemComponent {
+
+}
