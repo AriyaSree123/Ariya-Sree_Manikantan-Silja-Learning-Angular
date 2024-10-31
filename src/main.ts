ff69e7f14087678178fc5e97d77a97f55c767c84
matt <mhaug@stclaircollege.ca>
2024-10-31 18:37:24 -0400

    Fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,9 @@ import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.c
 import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
 const routes: Routes =[
-  {path:'', redirectTo: '/foods', component: FoodListComponent},
-  {path:'foods', component: FoodInfoComponent},
+  {path:'', redirectTo: '/foods', pathMatch: 'full'},
+  {path:'foods', component: FoodListComponent},
+  {path: 'food-info', component: FoodInfoComponent},
   {path:'modify-list-item', component: ModifyListItemComponent},
   {path:'**', component: PageNotFoundComponent}
 ];
