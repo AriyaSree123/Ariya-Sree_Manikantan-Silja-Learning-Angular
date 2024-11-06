4be9c3f4abc4c1639aa338a2c62fcbd70d466a85
Ariya Sree <W0849647@myscc.ca>
2024-11-06 17:44:10 -0500

    added edit and delete buttons

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,15 +1,20 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FoodProduction} from "../shared/models/food-production";
 import {FoodInfoComponent} from "../food-info/food-info.component";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {FoodService} from "../services/food.service";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {FoodList} from "../shared/models/dataMock-food";
 
 @Component({
   selector: 'app-food-list',
   standalone: true,
   imports: [
     FoodInfoComponent,
-    NgForOf
+    NgForOf,
+    RouterLink,
+    NgOptimizedImage,
+    NgIf
   ],
   templateUrl: './food-list.component.html',
   styleUrl: './food-list.component.css'
@@ -18,7 +23,9 @@ export class FoodListComponent implements OnInit{
   foodList: FoodProduction [] = [];
   @Input() food!: FoodProduction;
 
-  constructor (private foodService: FoodService){
+  constructor (private foodService: FoodService,
+               private route: ActivatedRoute,
+               private router: Router){
   }
 
   ngOnInit(): void {
@@ -26,5 +33,24 @@ export class FoodListComponent implements OnInit{
       next: (data: FoodProduction[]) => this.foodList = data
     })
   }
+
+  onEdit(ProductId: number){
+    this.router.navigate(['/modify-list-item']);
+  }
+
+  selectedFood?:FoodProduction;
+  selectFood(food:FoodProduction):void{
+    this.selectedFood = food;
+  }
+
+  onDelete(ProductId: number){
+    if(this.selectedFood){
+      this.foodService.deleteFood(this.selectedFood.ProductId);
+      this.foodList = this.foodList.filter(food=> food.ProductId !== this.selectedFood?.ProductId)
+      this.router.navigate(['/foods']);
+    }
+  }
+
+  protected readonly FoodList = FoodList;
 
 }
